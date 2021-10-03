390aafb85545c873b2baecc2c4db0a688e51c40f
Hana <hana.zupan@hotmail.com>
2021-10-03 10:29:38 +0200

    Prim algorithm working, also added random generation.

--- Maze/create_mazes.py
+++ Maze/create_mazes.py
@@ -18,6 +18,7 @@ class Maze:
         :param width:
         :param algorithm:
         """
+        self.algorithm = algorithm
         self.size = (height, width)
         self.maze = np.full(self.size, 2)
         # list of images for animation
@@ -26,6 +27,9 @@ class Maze:
             self._create_handmade()
         elif algorithm == 'Prim':
             self._create_prim()
+        elif algorithm == 'random':
+            self.maze = np.random.rand(height, width)
+            self.maze = np.round(self.maze)
         else:
             raise AttributeError("Not a valid algorithm choice.")
 
@@ -52,40 +56,56 @@ class Maze:
             print("Please, initialize a larger maze.")
 
     def _create_prim(self):
+        hall_list = []
+        wall_list = []
+
+        def create_hall(cell):
+            self.maze[cell] = 0
+            hall_list.append(cell)
+
+        def create_wall(cell):
+            self.maze[cell] = 1
+            wall_list.append(cell)
+
         # pick a random cell as a starting point and turn it into a hall
         height, width = self.size
         random_cell = np.random.randint(height), np.random.randint(width)
-        self.maze[random_cell] = 0
+        create_hall(random_cell)
+        assert len(hall_list) == 1
+        # for video
         self.image_list.append(self.maze.copy())
         # add walls of the cell to the wall list (periodic boundary conditions)
-        wall_list = self._determine_neighbours_periodic(random_cell)
-        for w in wall_list:
-            self.maze[w] = 1
+        neighbours = self._determine_neighbours_periodic(random_cell)
+        for n in neighbours:
+            create_wall(n)
         assert len(wall_list) == 4
         while len(wall_list) > 0:
             random_wall = random.choice(wall_list)
             neighbours = self._determine_neighbours_periodic(random_wall)
             values = [self.maze[l, c] for (l, c) in neighbours]
-            known_hall = neighbours[values.index(0.0)]
-            assert len(neighbours) == len(values) == 4
-            # if the opposite site 3 walls
-            opposite_side = self._determine_opposite(random_wall, known_hall)
-            if self.maze[opposite_side] == 2:
-                # make this wall a hall
-                self.maze[random_wall[0], random_wall[1]] = 0
-                # add directly neighbouring walls (not halls) to the wall_list
-                new_walls = [n for n, v in zip(neighbours, values) if v]
-                for w in new_walls:
-                    self.maze[w] = 1
-                wall_list.extend(new_walls)
+            for n in neighbours:
+                if n in hall_list:
+                    known_hall = n
+                    opposite_side = self._determine_opposite(random_wall, known_hall)
+                    if self.maze[opposite_side] == 2 and values.count(0) == 1:
+                        # make this wall a hall
+                        create_hall(random_wall)
+                        # add directly neighbouring empty cells to the wall_list
+                        new_walls = [n for n in neighbours if self.maze[n] == 2]
+                        for w in new_walls:
+                            create_wall(w)
             wall_list.remove(random_wall)
             self.image_list.append(self.maze.copy())
-
+        self.maze[self.maze == 2] = 1
+        self.image_list.append(self.maze.copy())
 
     def animation_building_maze(self):
+        if self.algorithm != 'Prim':
+            print("Animation only available for Prim algorithm.")
+            return
         # for producing a video
         fig = plt.figure()
-        im = plt.imshow(self.image_list[0], animated=True) #cmap='Greys',
+        im = plt.imshow(self.image_list[0], cmap='Greys', animated=True)
 
         def updatefig(i):
             im.set_array(self.image_list[i])
@@ -96,7 +116,7 @@ class Maze:
         anim = animation.FuncAnimation(fig, updatefig, blit=True, frames=len(self.image_list),
                                        repeat=False, interval=50)
         plt.show()
-        writergif = animation.PillowWriter(fps=10)
+        writergif = animation.PillowWriter(fps=30)
         anim.save("Images/animation.gif", writer=writergif)
 
     def _determine_neighbours_periodic(self, cell):
@@ -107,33 +127,17 @@ class Maze:
             (line, (column + 1) % width),
             ((line + 1) % height, column),
             (line, (column - 1) % width)
-            #((line - 1) % height, (column - 1) % width),
-            #((line - 1) % height, (column + 1) % width),
-            #((line + 1) % height, (column + 1) % width),
-            #((line + 1) % height, (column - 1) % width),
-
         ]
         return neighbours
 
     def _determine_opposite(self, central, known_hall):
         height, width = self.size
         if central[0] == known_hall[0]:
-            return (central[0], (2*central[1] - known_hall[1]) % width)
+            return central[0], (2*central[1] - known_hall[1]) % width
         elif central[1] == known_hall[1]:
-            return ((2*central[0] - known_hall[0]) % height, central[1])
+            return (2*central[0] - known_hall[0]) % height, central[1]
         else:
             raise ValueError("They are not neighbouring cells.")
-
-    def _determine_direct_neighbours(self, line, column):
-        height, width = self.size
-        neighbours = [
-            ((line - 1) % height, column),
-            (line, (column + 1) % width),
-            ((line + 1) % height, column),
-            (line, (column + 1) % width)
-        ]
-        return neighbours
-
 
     def visualize(self, save_as=None):
         """
@@ -152,7 +156,7 @@ class Maze:
 
 
 if __name__ == '__main__':
-    maze = Maze(20, 30)
-    #print(maze)
-    #maze.visualize()
+    maze = Maze(20, 30, algorithm = 'random')
+    print(maze)
+    maze.visualize()
     maze.animation_building_maze()
--- Maze/test_mazes.py
+++ Maze/test_mazes.py
@@ -7,6 +7,49 @@ class MazeTestCase(unittest.TestCase):
         test_maze = Maze(17, 8)
         self.assertEqual(test_maze.maze.shape, (17, 8))
 
+    def test_neighbours(self):
+        test_maze = Maze(20, 30)
+        # test a cell in the middle
+        chosen_cell = (5, 3)
+        neig = test_maze._determine_neighbours_periodic(chosen_cell)
+        assert len(neig) == 4
+        correct_neig = [(5, 2), (5, 4), (4, 3), (6, 3)]
+        for el in correct_neig:
+            self.assertIn(el, neig)
+        # also test periodic boundary
+        chosen_cell = (0, 29)
+        neig = test_maze._determine_neighbours_periodic(chosen_cell)
+        assert len(neig) == 4
+        correct_neig = [(0, 28), (0, 0), (1, 29), (19, 29)]
+        for el in correct_neig:
+            self.assertIn(el, neig)
+
+    def test_opposite(self):
+        test_maze = Maze(6, 10)
+        # hall left
+        central = (5, 3)
+        known_hall = (5, 2)
+        correct_opposite = (5, 4)
+        opposite = test_maze._determine_opposite(central, known_hall)
+        self.assertEqual(opposite, correct_opposite)
+        # hall below
+        known_hall = (6, 3)
+        correct_opposite = (4, 3)
+        opposite = test_maze._determine_opposite(central, known_hall)
+        self.assertEqual(opposite, correct_opposite)
+        # edge cases - hall above
+        central = (0, 2)
+        known_hall = (5, 2)
+        correct_opposite = (1, 2)
+        opposite = test_maze._determine_opposite(central, known_hall)
+        self.assertEqual(opposite, correct_opposite)
+        # edge cases - hall right
+        central = (5, 9)
+        known_hall = (5, 0)
+        correct_opposite = (5, 8)
+        opposite = test_maze._determine_opposite(central, known_hall)
+        self.assertEqual(opposite, correct_opposite)
+
 
 if __name__ == '__main__':
     unittest.main()
