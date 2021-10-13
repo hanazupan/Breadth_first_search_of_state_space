c976b4334610fd421d15868793c7d7b6ead99f33
Hana Zupan <hana.zupan@hotmail.com>
2021-10-13 10:22:25 +0200

    Created energy surfaces from mazes.

--- Maze/create_energies.py
+++ Maze/create_energies.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
 from collections.abc import Sequence
-import cmath
+from mpl_toolkits import mplot3d
 
 
 class AbstractEnergy(ABC):
@@ -55,14 +55,25 @@ class Energy:
         self.h = 1
         self.S = 1
         self.V = 1
+        # and let´s assume room temperature (does that make sense?)
+        self.T = 293  # 293K <==> 20°C
+        # energy cutoff - let's see if it should be changed
+        self.energy_cutoff = 0.5
+        # will only have a value if energy created from maze
+        self.underlying_maze = None
+        # in preparation
+        self.grid_x = None
+        self.grid_y = None
+        self.energies = None
+        self.size = None
 
-    def from_maze(self, maze: Maze, add_noise=True):
-        # for now let's assume the maze to be 2D
+    def from_maze(self, maze: Maze, add_noise=True, factor_grid=2):
+        # for now let's assume the maze to be 2D TODO: change this to enable any dimensionality
         # sparse grid
         size_x, size_y = complex(maze.size[0]), complex(maze.size[1])
         x_edges, y_edges = np.mgrid[-1:1:size_x, -1:1:size_y]
         # dense grid
-        x_dense, y_dense = np.mgrid[-1:1:5*size_x, -1:1:5*size_y]
+        self.grid_x, self.grid_y = np.mgrid[-1:1:factor_grid*size_x, -1:1:factor_grid*size_y]
         z = maze.maze
         # change some random zeroes into -1 and -2
         if add_noise:
@@ -72,19 +83,43 @@ class Energy:
             for _ in range(round(0.01*maze.size[0]*maze.size[1])):
                 cell = maze.find_random_accessible()
                 z[cell] = -2
+        self.underlying_maze = z
+        m = max(maze.size)
+        tck = interpolate.bisplrep(x_edges, y_edges, z, nxest=10*m, nyest=10*m, task=-1, tx=self.grid_x[:, 0], ty=self.grid_y[0, :])
+        znew = interpolate.bisplev(self.grid_x[:, 0], self.grid_y[0, :], tck)
+        self.energies = znew
+        self.size = znew.shape
+
+    def visualize_underlying_maze(self):
+        if not np.any(self.underlying_maze):
+            raise ValueError("No underlying maze present! This is only available for surfaces created from mazes.")
         lims = dict(cmap='RdBu_r', vmin=-2, vmax=1)
-        plt.pcolormesh(x_edges, y_edges, z, **lims)
+        size_x, size_y = complex(maze.size[0]), complex(maze.size[1])
+        x_edges, y_edges = np.mgrid[-1:1:size_x, -1:1:size_y]
+        plt.pcolormesh(x_edges, y_edges, self.underlying_maze, **lims)
         plt.show()
-        m = maze.size[0]
-        print(m - np.sqrt(2 * m), m + np.sqrt(2 * m))
-        tck = interpolate.bisplrep(x_edges, y_edges, z, s=0)
-        znew = interpolate.bisplev(x_dense[:, 0], y_dense[0, :], tck)
-        plt.pcolormesh(x_dense, y_dense, znew, **lims)
+
+    def visualize(self):
+        if not np.any(self.energies):
+            raise ValueError("No energies present! First, create an energy surface (e.g. from a maze).")
+        lims = dict(cmap='RdBu_r', vmin=-2, vmax=1)
+        plt.pcolormesh(self.grid_x, self.grid_y, self.energies, **lims)
         plt.colorbar()
+        plt.show()
+
+    def visualize_3d(self):
+        if not np.any(self.energies):
+            raise ValueError("No energies present! First, create an energy surface (e.g. from a maze).")
+        ax = plt.axes(projection='3d')
+        ax.plot_surface(self.grid_x, self.grid_y, self.energies, rstride=1, cstride=1,
+                        cmap='RdBu_r', edgecolor='none')
         plt.show()
 
 
 if __name__ == '__main__':
     energy = Energy()
-    maze = Maze((22, 26))
-    energy.from_maze(maze, add_noise=False)
+    maze = Maze((12, 16))
+    energy.from_maze(maze, add_noise=True)
+    energy.visualize()
+    #energy.visualize_underlying_maze()
+    energy.visualize_3d()
